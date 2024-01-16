91b1816d6d674255260e48b11b66487677629820
yangharry <myid7771@naver.com>
2024-01-16 12:35:46 +0900

    admin, signin approve pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import Main from './pages/main/Main';
 import Owner from './pages/owner/Owner';
+import Admin from './pages/admin/Admin';
 
 function App() {
   return (
     <Routes>
-      <Route path="*" element={<div>404 page</div>}></Route>
-      <Route path="/" element={<Main />}></Route>
-      <Route path="/owner" element={<Owner />}></Route>
+      <Route path="*" element={<div>404 page</div>} />
+      <Route path="/" element={<Main />} />
+      <Route path="/owner" element={<Owner />} />
+      <Route path="/admin" element={<Admin />} />
     </Routes>
   );
 }
